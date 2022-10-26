6816275cd9347ae1e6f8d27d652e6ad4430327ed
neebla <1445565922@qq.com>
2022-10-26 14:40:10 +0800

    feat: 通过设置data-theme改变主题

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,60 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const currentTheme = ref(localStorage.getItem('theme') || 'light')
+const changeTheme = (theme: string) => {
+  localStorage.setItem('theme', theme)
+  currentTheme.value = theme
+  window.document.documentElement.setAttribute('data-theme', theme) // key
+}
+changeTheme(currentTheme.value)
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="theme-block">
+    <div
+      class="light"
+      v-bind:class="{ active: currentTheme == 'light' }"
+      @click="changeTheme('light')"
+    >
+      Light
     </div>
-  </header>
-
-  <RouterView />
+    <span>|</span>
+    <div
+      class="dark"
+      :class="{ active: currentTheme == 'dark' }"
+      @click="changeTheme('dark')"
+    >
+      Dark
+    </div>
+  </div>
+  <div class="demo-wrapper">
+    Demo
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style lang="scss" scoped>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+.theme-block {
+  display: flex;
+  align-items: center;
+  background: var(--background);
+  border-radius: 1em;
+  width: fit-content;
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  & > div {
+    margin: 0.5em;
+    cursor: pointer;
+    color: #e9ccd3;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  .active {
+    color: var(--color);
   }
+}
+.demo-wrapper {
+  margin-top: 24px;
+  padding: 12px;
+  background: var(--background);
+  color: var(--color);
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
